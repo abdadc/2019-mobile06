8448a60d88e4fdd642f63ede4cb24c3c351a857e
abdadc <auri.culare@yahoo.com>
2019-10-11 00:18:26 +0700

    Menambahkan BodyMassFragment dan membuat proses Body Mass Index

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -8,31 +8,34 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
+import id.ac.polinema.idealbodyweight.fragments.BodyMassFragment;
 import id.ac.polinema.idealbodyweight.fragments.BrocaIndexFragment;
 import id.ac.polinema.idealbodyweight.fragments.MenuFragment;
 import id.ac.polinema.idealbodyweight.fragments.ResultFragment;
 
-public class MainActivity extends AppCompatActivity implements
-		MenuFragment.OnFragmentInteractionListener, BrocaIndexFragment.OnFragmentInteractionListener,
-		ResultFragment.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener,
+		BrocaIndexFragment.OnFragmentInteractionListener, ResultFragment.OnFragmentInteractionListener,
+		BodyMassFragment.OnFragmentInteractionListener {
 
-	// Deklarasikan atribut Fragment di sini
-	private ResultFragment resultFragment;
-	MenuFragment menuFragment;
-	private BrocaIndexFragment brocaIndexFragment;
 	private AboutFragment aboutFragment;
+	private MenuFragment menuFragment;
+	private BrocaIndexFragment brocaIndexFragment;
+	private BodyMassFragment bodyMassIndexFragment;
+	private ResultFragment resultFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		aboutFragment = AboutFragment.newInstance("Abdallah DC");
+
 		menuFragment = new MenuFragment();
 		getSupportFragmentManager().beginTransaction()
 				.replace(R.id.fragment_container, menuFragment)
 				.commit();
-		brocaIndexFragment = new BrocaIndexFragment();
 
+		brocaIndexFragment = new BrocaIndexFragment();
+		bodyMassIndexFragment = new BodyMassFragment();
 		resultFragment = new ResultFragment();
 	}
 
@@ -44,14 +47,12 @@ public class MainActivity extends AppCompatActivity implements
 
 	@Override
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-		// TODO: Tambahkan penanganan menu di sini
 		if (item.getItemId() == R.id.menu_about) {
 			getSupportFragmentManager().beginTransaction()
 					.replace(R.id.fragment_container, aboutFragment)
 					.addToBackStack(null)
 					.commit();
 		}
-
 		return super.onOptionsItemSelected(item);
 	}
 
@@ -64,7 +65,9 @@ public class MainActivity extends AppCompatActivity implements
 
 	@Override
 	public void onBodyMassIndexButtonClicked() {
-
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, bodyMassIndexFragment)
+				.commit();
 	}
 
 	@Override
@@ -79,6 +82,14 @@ public class MainActivity extends AppCompatActivity implements
 	public void onTryAgainButtonClicked(String tag) {
 		getSupportFragmentManager().beginTransaction()
 				.replace(R.id.fragment_container, brocaIndexFragment)
+				.commit();
+	}
+
+	@Override
+	public void onCalculateBodyMassIndexClicked(float index) {
+		resultFragment.setInformation(String.format("Your Body Mass Index is %.2f", index));
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, resultFragment, "BODY_MASS_INDEX")
 				.commit();
 	}
 }
